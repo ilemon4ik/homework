fb8fe26e95fe6ea15f6a0839ddc4a5db5870a6c8
Lemon <ilya2808199915@gmail.com>
2025-05-04 20:05:12 +0300

    домашнее задание

--- src/Lesson_two.kt
+++ src/Lesson_two.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun  main(){
     lesson_two_one()
 }
@@ -16,3 +18,52 @@ fun lesson_two_one(){
     println("Периметр прямоугольника: $perimeter")
     println("Площадь прямоугольника: $area")
 }
+
+fun lesson_two_two(){
+
+    println("Введите ваше кол-во баллов: ")
+    val point = readLine()!!.toInt()
+
+    if (point >= 90 && point <= 100) {
+        println("Оценка А:" + point)
+    } else if (point >= 75) {
+        println("Оценка B:" + point)
+    } else if (point >= 50) {
+        println("Оценка C:" + point)
+    } else if (point >= 30) {
+        println("Оценка D:" + point)
+    } else if (point >= 0) {
+        println("Оценка F:" + point)
+    }
+}
+
+fun lesson_two_three(){
+
+    println("Введите число N")
+    val numberN = readLine()!!.toInt()
+
+    for (i in 1..10) {
+        println(numberN.toString() + " * " + i + " = " + numberN * i)
+    }
+}
+
+fun lesson_two_dopzadanie() {
+
+    var condition = true
+
+    do {
+        println("Введите число")
+        val number = readLine()!!.toInt()
+        if (number < 0) {
+            condition = false
+        } else {
+            println("Вы ввели число: " + number)
+        }
+    } while (condition)
+
+    println("Число отрицательное, программа завершилась!")
+
+}
+
+
+
