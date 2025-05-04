f5fcf19c8da9790d1db13ff44a58b2c33280f635
Lemon <ilya2808199915@gmail.com>
2025-05-04 20:05:09 +0300

    домашнее задание

--- src/Lesson_two.java
+++ src/Lesson_two.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Lesson_two {
     public static void main (String[] args){
-        lesson_two_one();
+        lesson_two_dopzadanie();
     }
+
     public static void lesson_two_one (){
 
         Scanner scanner = new Scanner(System.in);
@@ -22,5 +23,52 @@ public class Lesson_two {
 
 
         scanner.close();
+    }
+
+    public static void lesson_two_two(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше кол-во баллов: ");
+        int point = scanner.nextInt();
+
+        if (point >= 90 && point <= 100) {
+            System.out.println("Оценка А:" + point);
+        } else if (point >= 75) {
+            System.out.println("Оценка B:" + point);
+        } else if (point >= 50) {
+            System.out.println("Оценка C:" + point);
+        } else if (point >= 30) {
+            System.out.println("Оценка D:" + point);
+        } else if (point >= 0) {
+            System.out.println("Оценка F:" + point);
+        }
+    }
+
+    public static void lesson_two_three() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число N");
+        int numberN = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(numberN + " * " + i + " = " + numberN * i);
+        }
+    }
+
+    public static void lesson_two_dopzadanie() {
+
+        boolean condition = true;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Введите число");
+            int number = scanner.nextInt();
+            if (number < 0) {
+                condition = false;
+            } else {
+                System.out.println("Вы ввели число: " + number);
+            }
+        } while (condition);
+            System.out.println("Число отрицательное, программа завершилась!");
     }
 }
