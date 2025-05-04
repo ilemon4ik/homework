4a711366840130d0d6921ff2906b5d108ace95a2
Lemon <ilya2808199915@gmail.com>
2025-05-04 15:02:28 +0300

    домашнее задание

--- src/Lesson_one.java
+++ src/Lesson_one.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Lesson_one {
 
     public static void main(String[] args) {
+        Lesson_one();
 
+    }
+    public static void Lesson_one(){
         System.out.println("Hello Java, (Введи своё имя)");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Hello Java, " + name);
     }
-    public static void two(String[] args){
+    public static void Lesson_two(){
         System.out.println("Good!");
     }
 }
